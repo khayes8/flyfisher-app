ab8fb5ebce28d3edbaf55d7a2756e6d1d250c302
donslovenkai <don.slovenkai@gmail.com>
2017-10-05 19:23:57 -0400

    Initial files for routes and db

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS flyfisher;
+
+CREATE DATABASE flyfisher;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--Seed data for Trip table--
+INSERT INTO Trip (trip_name, location, trip_date) VALUES ('Spring Trip to Lake Francis', 'Pittsburg, NH', '4/25/17');
+INSERT INTO Trip (trip_name, location, trip_date) VALUES ('Summer Trip to First Lake', 'Pittsburg, NH', '7/7/17');
+--Seed data for Fish table--
+INSERT INTO Fish (fish_name, bait, notes) VALUES ('Old Speckled Trout', 'dry fly', 'A very big fish!');
+INSERT INTO Fish (fish_name, bait, notes) VALUES ('Billy Bass', 'wet fly', 'A very funky fish!');
