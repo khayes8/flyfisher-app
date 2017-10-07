898f92b2b62a17e13f42021b2eeb2bf7e524b545
khayes8 <khayes8@lesley.edu>
2017-10-07 12:23:34 -0400

    Add files via upload

---
+++ models/trip.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+
+  var Trip = sequelize.define("Trip", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    trip_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    // trip_date: {
+    //   isDate: true,
+    //   allowNull: false
+    // }
+  });
+
+  Trip.associate = function(models) {
+    Trip.hasMany(models.Fish, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Trip;
+};
