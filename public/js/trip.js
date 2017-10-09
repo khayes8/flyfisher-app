bd4068ccbb60ce9f3393df7e056adb6b4b1bbd0a
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-10-09 17:17:54 -0400

    changed data to table

--- public/js/trip.js
+++ public/js/trip.js
@@ -1,35 +1,3 @@
-//need event listener ✔
-//user clicks update 
-//disable update button ✔
-//hide data ✔
-//form fields appear and submit button appears ✔
-
-//when user clicks submit, store information from fields and store it in an object
-//run ajax request to update the database at that id
-
-// $(document).ready(function() {
-//     var title        = $(".title"); //Fields wrapper
-//     var update      = $(".update"); //Add button ID
-//     var i = 1; //initlal text box count
-// 	$("#submitChanges").hide();
-// 	$(".formb").hide();
-// 	$("remove_field").hide();
-
-//    $(update).click(function(){ //on add input button click
-    
-//     //hide and disable elements
-//     // $(".span").hide();
-//     $(".update").prop("disabled",true);
-//     $("#submitChanges").show();
-//     $(".formb").show();
-//     $("remove_field").hide();
-// 	});
-
-//    $("#submitChanges").click(function(){
-//    	alert("hi");
-//    })
-// });
-
 
 
 
@@ -49,12 +17,23 @@ $('#edit').click(function(){
 $('#save').click(function(){
   $('#save, .info').hide();
   $('textarea').each(function(){
-    var content = $(this).val();//.replace(/\n/g,"<br>");
+    var content = $(this).val();
+    console.log(content);//.replace(/\n/g,"<br>");
     $(this).html(content);
     $(this).contents().unwrap();    
   }); 
-
+  // updateDB(content);
   $('#edit').show(); 
 });
 
-
+// function updateDB(content){
+// 	$.ajax({
+//       method: "PUT",
+//       url: "/trips",
+//       data: content
+//     }).done(){
+// 		$.get("/trips", function(data) {
+// console.log(data)
+//     });
+//     }
+//   }
