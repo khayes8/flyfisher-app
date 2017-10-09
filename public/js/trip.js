5ae6e5a204e1296ba4aa6ee9c460d987205e6243
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-10-09 14:17:29 -0400

    trying to set up onclick function for submit button to take in data, having issues

--- public/js/trip.js
+++ public/js/trip.js
@@ -8,16 +8,14 @@
 //run ajax request to update the database at that id
 
 $(document).ready(function() {
-    var max_fields      = 1; //maximum input boxes allowed
     var title        = $(".title"); //Fields wrapper
     var update      = $(".update"); //Add button ID
     var i = 1; //initlal text box count
 	$("#submitChanges").hide();
 	$(".formb").hide();
 	$("remove_field").hide();
-    
 
-    $(update).click(function(e){ //on add input button click
+	   $(update).click(function(e){ //on add input button click
         e.preventDefault();
         //hide and disable elements
         $(".span").hide();
@@ -25,16 +23,12 @@ $(document).ready(function() {
         $("#submitChanges").show();
         $(".formb").show();
         $("remove_field").hide();
-
-    //JUST BEFORE END OF FUNCTION DO NOT DELETE QUOTES BELOW
     });
 
-    
-    $(title).on("click",".remove_field", function(e){ //user click on remove text
-        e.preventDefault(); $(this).parent('div').remove(); x--;
-        // $(".span").show();
-    })
-
-
+	   // $("#submitChanges").click(function()){
+	   // 	alert("hi");
+	   // }
 });
+
+
 
