9e1e97ccf277ed0477670fb6efc7646a3904a07e
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-10-09 13:04:17 -0400

    form is finally starting to come together

--- public/js/trip.js
+++ public/js/trip.js
@@ -1,9 +1,42 @@
-$('.update').click(function(){
-$( ".span" ).append( <form action="">
-  First name: <input type="text" name="fname"><br>
-  <input type="text" name="lname"><br>
-  <input type="submit" value="Submit">
-</form> );
-});
+//need event listener ✔
+//user clicks update 
+//disable update button ✔
+//hide data ✔
+//form fields appear and submit button appears ✔
 
+//when user clicks submit, store information from fields and store it in an object
+//run ajax request to update the database at that id
+
+$(document).ready(function() {
+    var max_fields      = 1; //maximum input boxes allowed
+    var title        = $(".title"); //Fields wrapper
+    var update      = $(".update"); //Add button ID
+    var i = 1; //initlal text box count
+	$("#submitChanges").hide();
+	$("form").hide();
+    
+
+    $(update).click(function(e){ //on add input button click
+        e.preventDefault();
+        //hide and disable elements
+        $(".span").hide();
+        $(".update").prop("disabled",true);
+        $("#submitChanges").show();
+        $("form").show();
+        //no more than one input field appended after text
+        // if(i <= max_fields){ //max input box allowed
+        //     i++; //text box increment
+        //     $(title).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        // }
+    //JUST BEFORE END OF FUNCTION DO NOT DELETE QUOTES BELOW
+    });
+
+    
+    $(title).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+        // $(".span").show();
+    })
+
+
+});
 
