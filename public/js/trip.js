ff3deb60ff199d8d7f72640bd2268436135666d3
Hongsyn <hongsynk@gmail.com>
2017-10-10 14:59:08 -0400

    Updated files to match master

--- public/js/trip.js
+++ public/js/trip.js
@@ -1,35 +1,3 @@
-//need event listener ✔
-//user clicks update
-//disable update button ✔
-//hide data ✔
-//form fields appear and submit button appears ✔
-
-//when user clicks submit, store information from fields and store it in an object
-//run ajax request to update the database at that id
-
-// $(document).ready(function() {
-//     var title        = $(".title"); //Fields wrapper
-//     var update      = $(".update"); //Add button ID
-//     var i = 1; //initlal text box count
-// 	$("#submitChanges").hide();
-// 	$(".formb").hide();
-// 	$("remove_field").hide();
-
-// 	   $(update).click(function(e){ //on add input button click
-//         e.preventDefault();
-//         //hide and disable elements
-//         // $(".span").hide();
-//         $(".update").prop("disabled",true);
-//         $("#submitChanges").show();
-//         $(".formb").show();
-//         $("remove_field").hide();
-//     });
-
-// 	   $("#submitChanges").click(function(){
-// 	   	alert("hi");
-// 	   })
-// });
-
 $('#edit').click(function(){
   $('#edit').hide();
   $('td').each(function(){
@@ -52,13 +20,21 @@ $('#save').click(function(){
   // updateDB(content);
   $('#edit').show();
 
-//   var location = $(".location").val().trim();
-//         // var trip_date = $("trip_date").val().trim();
-//         var fish_name = $(".fish_name").val().trim();
-//         var bait = $(".bait").val().trim();
-//         var notes = $(".notes").val().trim();
-//   // updateDB(location, fish_name, bait, notes);
-// });
+  var location = $(".location").val().trim();
+        // var trip_date = $("trip_date").val().trim();
+        var fish_name = $(".fish_name").val().trim();
+        var bait = $(".bait").val().trim();
+        var notes = $(".notes").val().trim();
+  // updateDB(location, fish_name, bait, notes);
+
+  $.ajax({
+    url: '/trips',
+    type: 'PUT',
+    // success: function(result){
+
+    // }
+  })
+});
 
 // function updateDB(location, fishName, bait, notes){
 //  var tripObj = {
--- routes/html-trip-routes.js
+++ routes/html-trip-routes.js
@@ -1,53 +1,53 @@
- var path = require("path");
-  var db = require("../models")
+var path = require("path");
+var db = require("../models")
 
-  module.exports = function(app) {
+module.exports = function(app) {
 
-    app.get("/", function(req, res) {
-      res.render("index");
-    });
+  app.get("/", function(req, res) {
+    res.render("index");
+  });
 
-    app.get("/create", function(req, res) {
-      res.render("form");
-    });
+  app.get("/create", function(req, res) {
+    res.render("form");
+  });
 
-    app.get("/trips", function(req, res) {
-      db.Trip.findAll({include: [db.Fish]}).then(data => {
-        var hbsObject = {
-          trips: data
-        };
-        console.log(hbsObject);
-        res.render("trip", hbsObject);
-      });
-    });
-
-
-    app.post("/trips", function(req, res) {
-      console.log(req.body);
-
-      db.Trip.create({
-      trip_name: req.body.trip_name,
-      location: req.body.location,
-      date: req.body.date,
-      // fish_name: req.body.fish_name,
-      // bait: req.body.bait,
-      // notes: req.body.notes
-    }).then(function(hbsObject) {
-      // Access created to the new trip as an argument inside of the callback function
+  app.get("/trips", function(req, res) {
+    db.Trip.findAll({include: [db.Fish]}).then(data => {
+      var hbsObject = {
+        trips: data
+      };
       console.log(hbsObject);
-      db.Fish.create({
-      TripId: hbsObject.id,
-      fish_name: req.body.fish_name,
-      bait: req.body.bait,
-      notes: req.body.notes
-      })
-      // res.end();
-      // res.render("trip", hbsObject);
-      res.redirect("/trips");
-      })
+      res.render("trip", hbsObject);
     });
+  });
 
-    app.delete("/trips", function(req, res) {
+
+  app.post("/trips", function(req, res) {
+    console.log(req.body);
+
+    db.Trip.create({
+    trip_name: req.body.trip_name,
+    location: req.body.location,
+    date: req.body.date,
+    // fish_name: req.body.fish_name,
+    // bait: req.body.bait,
+    // notes: req.body.notes
+  }).then(function(hbsObject) {
+    // Access created to the new trip as an argument inside of the callback function
+    console.log(hbsObject);
+    db.Fish.create({
+    TripId: hbsObject.id,
+    fish_name: req.body.fish_name,
+    bait: req.body.bait,
+    notes: req.body.notes
+    })
+    // res.end();
+    // res.render("trip", hbsObject);
+    res.redirect("/trips");
+    })
+  });
+
+  app.delete("/trips", function(req, res) {
     // We just have to specify which todo we want to destroy with "where"
     db.Trip.destroy({
       where: {
@@ -63,9 +63,9 @@
     res.redirect("/trips");
     });
 
-    });
+  });
 
-    app.put("/trips", function(req, res) {
+  app.put("/trips", function(req, res) {
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
     db.Trip.update({
@@ -84,7 +84,7 @@
       notes: req.body.notes
     });
       res.redirect("/trips");
-  });
+    });
   });
 
-  }
+}
