408b1fc40649a1ef73fc257fc884cd6c07a70343
Dave Jagodowski <jagodowski@gmail.com>
2017-10-09 21:52:05 -0400

    Add files via upload

---
+++ routes/html-trip-routes.js
@@ -0,0 +1,90 @@
+  var path = require("path");
+  var db = require("../models")
+
+  module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+      res.render("index");
+    });
+
+    app.get("/create", function(req, res) {
+      res.render("form");
+    });
+
+    app.get("/trips", function(req, res) {
+      db.Trip.findAll({include: [db.Fish]}).then(data => {
+        var hbsObject = {
+          trips: data
+        };
+        console.log(hbsObject);
+        res.render("trip", hbsObject);
+      });
+    });
+
+
+    app.post("/trips", function(req, res) {
+      console.log(req.body);
+
+      db.Trip.create({
+      trip_name: req.body.trip_name,
+      location: req.body.location,
+      date: req.body.date,
+      // fish_name: req.body.fish_name,
+      // bait: req.body.bait,
+      // notes: req.body.notes
+    }).then(function(hbsObject) {
+      // Access created to the new trip as an argument inside of the callback function
+      console.log(hbsObject);
+      db.Fish.create({
+      TripId: hbsObject.id,
+      fish_name: req.body.fish_name,
+      bait: req.body.bait,
+      notes: req.body.notes
+      })
+      // res.end();
+      // res.render("trip", hbsObject);
+      res.redirect("/trips");
+      })
+    });
+
+    app.delete("/trips", function(req, res) {
+    // We just have to specify which todo we want to destroy with "where"
+    db.Trip.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbTodo) {
+      db.Fish.destroy({
+      TripId: hbsObject.id,
+      fish_name: req.body.fish_name,
+      bait: req.body.bait,
+      notes: req.body.notes
+    });
+    res.redirect("/trips");
+    });
+
+    });
+
+    app.put("/trips", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Trip.update({
+      trip_name: req.body.trip_name,
+      location: req.body.location,
+      date: req.body.date
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(hbsObject) {
+      db.Fish.update({
+      TripId: hbsObject.id,
+      fish_name: req.body.fish_name,
+      bait: req.body.bait,
+      notes: req.body.notes
+    });
+      res.redirect("/trips");
+  });
+  });
+    
+  }
--- routes/trip-routes.js
+++ routes/trip-routes.js
@@ -1,5 +1,56 @@
-//trip-routes.js
+//routes/trip-routes.js
+//Routes for displaying data and saving to the flyfisher db, Trip table
 
-// Routes
+var db = require("../models");
+// TRIP ROUTES
+// =================================/============================================
 module.exports = function(app) {
-}
+  // GET route for getting all of trips
+  app.get("/api/trip", function(req, res) {
+    // findAll returns all entries for the Trip table when used with no options
+    db.Trip.findAll({include: [db.Fish]}).then(function(dbTrip) {
+      // Access to the trips as an argument inside of the callback function
+      console.log(dbTrip);
+      res.json(dbTrip);
+    });
+  });
+  // POST route for saving a new trip
+  app.post("/api/trip", function(req, res) {
+    console.log(req.body);
+    // Create takes an argument of an object describing the item to insert to Trips table.
+    // Pass in an object with text and complete property (req.body)
+    db.Trip.create({
+      text: req.body.text,
+      complete: req.body.complete
+    }).then(function(dbTrip) {
+      // Access created to the new trip as an argument inside of the callback function
+      res.json(dbTrip);
+    });
+  });
+  // DELETE route for deleting a trip
+  app.delete("/api/trip/:id", function(req, res) {
+    // Specifies which trip to destroy using "where"
+    db.Trip.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbTrip) {
+      res.json(dbTrip);
+    });
+  });
+  // PUT route for updating trips. Grip updated trip  data from req.body
+  app.put("/api/trip", function(req, res) {
+    // Update takes in an object describing the properties to update; where
+    // describes the objects to update
+    db.Trip.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbTrip) {
+      res.json(dbTrip);
+    });
+  });
+};
