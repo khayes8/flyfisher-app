783707502ece3ef77a23b1a4ee752e16e2ff8242
DaveJag <davejag@comcast.net>
2017-10-10 09:51:40 -0400

    Don's updated fish-routes.js file

--- routes/fish-routes.js
+++ routes/fish-routes.js
@@ -17,11 +17,12 @@ module.exports = function(app) {
   // POST route for saving a new fish
   app.post("/api/fish", function(req, res) {
     console.log(req.body);
-    // Create takes an argument of an object describing the item to insert to fishs table.
-    // Pass in an object with text and complete property (req.body)
+    // Create takes an argument of an object describing the item to insert to fish table.
+    // Pass in an object with Fish table properties (req.body)
     db.Fish.create({
-      text: req.body.text,
-      complete: req.body.complete
+      fish_name: req.body.fish_name,
+      bait: req.body.bait,
+      notes: req.body.notes
     }).then(function(dbFish) {
       // Access created to the new fish as an argument inside of the callback function
       res.json(dbFish);
@@ -43,14 +44,22 @@ module.exports = function(app) {
     // Update takes in an object describing the properties to update; where
     // describes the objects to update
     db.Fish.update({
-      text: req.body.text,
-      complete: req.body.complete
+      fish_name: req.body.fish_name,
+      bait: req.body.bait,
+      notes: req.body.notes
     }, {
       where: {
         id: req.body.id
       }
+// Optional PUT route that doesn't explicitly specify the properties
+// app.put("/api/fish", function(req, res) {
+//    db.Fish.update(req.body,
+//       {
+//          where: {
+//            id: req.body.id
+//        }     
     }).then(function(dbFish) {
       res.json(dbFish);
     });
   });
-}
+};
